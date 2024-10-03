1a60b8ed2c224ecff018d1f17b26707b2437f484
Klaci00 <kenyolaszlo6@gmail.com>
2024-10-03 11:58:44 +0200

    4-es 5-ös feladat kész

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BukkMaraton2019;
+using System.Reflection;
+using System.Runtime.ExceptionServices;
+
+string dir = Directory.GetCurrentDirectory();
+Console.WriteLine(dir);
+
+string filepath = "./2019_bukkmaraton_eredmeny.csv";
+
+FileStream fileStream = File.Open(path:filepath,FileMode.Open,FileAccess.Read,FileShare.ReadWrite);
+BufferedStream bufferedStream = new BufferedStream(fileStream);
+StreamReader reader = new StreamReader(bufferedStream);
+
+List<Versenytav> adatok = new List<Versenytav>();
+
+string line;
+
+while ((line = reader.ReadLine()) != null)
+{
+    string[] strings = line.Split(';');
+    string rajtszam = strings[0];
+    string kategoria = strings[1];
+    string nev=strings[2];
+    string egyesulet=strings[3];
+    string ido = strings[4];
+    if (kategoria!="Kategória "){
+        Versenytav versenytav = new(rajtszam:rajtszam,kategoria: kategoria,nev:nev,egyesulet:egyesulet,ido:ido);
+        adatok.Add(versenytav);
+    }
+}
+
+Console.WriteLine("4-es feladat");
+
+int DNF = adatok.Count(x => x.Ido == "Nem teljesítő");
+
+double DNFratio = (Convert.ToDouble(DNF) / adatok.Count())*100;
+
+Console.WriteLine($"A versenyzők {Math
+    .Round(DNFratio,2)}%-a nem teljesítette a versenyt.");
+
+Console.WriteLine("5-ös feladat");
+
+int femaleShort = adatok.Count(x=>x.Nem()=="Nő" && x.Tav()=="Rövidtáv");
+
+Console.WriteLine($"A rövidtávon induló nők létszáma: {femaleShort}");
---
+++ Versenytav.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BukkMaraton2019
+{
+    public class Versenytav
+    {
+        public string Rajtszam { get; set; }
+        public string Kategoria { get; set; }
+        public string Nev {  get; set; }
+        public string Egyesulet { get; set; }
+        public string Ido {  get; set; }
+        public Versenytav(string rajtszam,string kategoria, string nev, string egyesulet, string ido) 
+        {
+            Rajtszam = rajtszam;
+            Kategoria = kategoria;
+            Nev = nev;
+            Egyesulet = egyesulet;
+            Ido = ido;
+
+        }
+
+        public string Tav()
+        {
+            char[] rajtsz=Rajtszam.ToCharArray();
+            char kategoria = rajtsz[0];
+            Dictionary<char, string> kategoriaDict = new()
+            {
+                {'R', "Rövidtáv"},
+                {'K', "Középtáv" },
+                {'H', "Hosszútáv" },
+                {'M', "Marathon" },
+                {'E', "E-bike" }
+            };
+        return kategoriaDict[kategoria] ; }
+
+        public string Nem()
+        {
+            char[] nem = Kategoria.Trim().ToCharArray();
+            
+            if (nem[nem.Length-1] == 'n')
+            {
+                return "Nő";
+            }
+            else return "Férfi";
+        }
+
+        public static int Getseconds(string time)
+        {
+            string[] parts=time.Split(':');
+            foreach (string part in parts)
+            {
+                foreach (char s in part)
+                {
+                    if (s != 0) break;
+                    else part.Remove(s);
+
+                }
+            }
+            Console.WriteLine(parts);
+            int hours = Convert.ToInt32( parts[0]);
+            int minutes = Convert.ToInt32( parts[1]);
+            int seconds = Convert.ToInt32( parts[2]);
+
+            int totalSeconds=hours*3600+minutes*60+seconds;
+            return totalSeconds;
+        }
+    }
+}
